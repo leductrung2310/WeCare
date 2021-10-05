9f162b8e48ac641d740714722501727e51082a92
thanhphat219 <phat01283249260@gmail.com>
2021-10-05 11:33:09 +0700

    Update Login Sign Up Profile

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xFF91c788);
 const accentColor = Color.fromRGBO(0, 0, 0, .7);
 const whiteColor = Colors.white;
+const grey1 = Color(0xFFf3f3f3);
+const greenLight = Color(0xFF91c777);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wecare_flutter/constants.dart';
+import 'package:wecare_flutter/screen/authentication/login/home_view_mode.dart';
+import 'package:wecare_flutter/screen/authentication/login/login_screen.dart';
+import 'package:wecare_flutter/screen/authentication/register/register_screen.dart';
+import 'package:wecare_flutter/screen/authentication/register/register_update_infor_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,28 +14,42 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     theme: ThemeData(
+  //       fontFamily: "Poppins",
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     home: const LoginScreen(),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "Poppins",
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => LoginViewModel(),
+      child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: "Poppins",
+          primaryColor: Color(0xFF91c788),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: RegisterUpdateInfoScreen(),
       ),
-      home: Home(),
     );
   }
 }
 
 class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("hello word"), 
-        backgroundColor: accentColor,)
-    );
+        appBar: AppBar(
+      title: const Text("hello word"),
+      backgroundColor: accentColor,
+    ));
   }
 }
